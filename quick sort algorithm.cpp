2b0d14c3e9ac037378b2f63e99bacc971916cb2e
komoh1511 <newer909more1510@mail.ru>
2024-05-08 20:00:13 +1200

    Add files via upload

---
+++ quick sort algorithm.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+
+void quickSort(std::vector<int>& a, int left, int right) {
+    int i = left;
+    int j = right;
+    int pivot = a[(left + right) / 2];
+
+    do {
+        while (a[i] < pivot) {
+            i++;
+        }
+        while (a[j] > pivot) {
+            j--;
+        }
+
+        if (i <= j) {
+            std::swap(a[i], a[j]);
+            i++;
+            j--;
+        }
+    } while (i <= j);
+
+    if (left < j) {
+        quickSort(a, left, j);
+    }
+    if (i < right) {
+        quickSort(a, i, right);
+    }
+}
+
+void printVector(std::vector<int> a) {
+    for (int i = 0; i < a.size(); i++) {
+        std::cout << a[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    std::cout << "¬ведите размер массива: ";
+    int size;
+    std::cin >> size;
+
+    std::vector<int> a(size);
+
+    // ¬вод данных
+    for (int i = 0; i < size; i++) {
+        std::cout << "¬ведите элемент массива: ";
+        std::cin >> a[i];
+    }
+
+    quickSort(a, 0, size - 1);
+
+    printVector(a);
+
+    return 0;
+}
